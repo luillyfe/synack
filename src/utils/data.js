9866d70933b9d5da45d1de8fac39ad11cb53b53a
Fermin Blanco <luillyfe89@gmail.com>
2020-10-21 14:30:22 -0500

    feat: search engine backbones.

---
+++ src/utils/data.js
@@ -0,0 +1,35 @@
+const data = [
+    {
+        img: "https://via.placeholder.com/150",
+        title: 'Image',
+        author: 'author',
+        cols: 2,
+    },
+    {
+        img: "https://via.placeholder.com/150",
+        title: 'Image',
+        author: 'author',
+        cols: 2,
+    },
+    {
+        img: "https://via.placeholder.com/150",
+        title: 'Image',
+        author: 'author',
+        cols: 2,
+    },
+    {
+        img: "https://via.placeholder.com/150",
+        title: 'Image',
+        author: 'author',
+        cols: 2,
+    },
+    {
+        img: "https://via.placeholder.com/150",
+        title: 'Image',
+        author: 'author',
+        cols: 2,
+    },
+
+];
+
+export default data;
